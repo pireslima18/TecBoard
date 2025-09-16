77cdad2b94f2fcab919182d85b31fb6538b464e9
Arthur Lima <arthurlima@192.168.0.206>
2025-09-16 10:24:08 -0300

    main: Subindo projeto TecBoard

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      <header>
+        <img src="/logo.png" alt="" />
+      </header>
+      <section>
+        <img src="/banner.png" alt="" />
+      </section>
+    </main>
+  )
+}
+
+export default App
